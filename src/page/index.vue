5b6b25da5210d840c1ce56ea07d3a7942d3afd81
jaysonreyes001 <jaysonreyes012345@gmail.com>
2025-11-26 22:30:38 +0800

    upload

--- src/page/index.vue
+++ src/page/index.vue
@@ -1,9 +1,9 @@
 <template lang="">
      <div class="flex justify-center mt-[200px] w-full ">
-          <div class="bg-[#101828] block min-w-[500px] p-6 border border-[#1e2939] rounded-md shadow-xs">
+          <div class="bg-[#101828] block lg:min-w-[500px] min-[100px] p-6 border border-[#1e2939] rounded-md shadow-xs">
 
-               <p v-if="flashcard =='' " class="text-center text-3xl font-semibold text-white mt-5 capitalize">click next to display flashcard</p>
-               <p v-else class="text-center text-5xl font-bold text-white mt-5 capitalize">{{flashcard?.romanji}}</p>
+               <p v-if="flashcard =='' " class="text-center text-xl lg:text-3xl font-semibold text-white mt-5 capitalize">click next to display flashcard</p>
+               <p v-else class="text-center text-3xl lg:text-5xl font-bold text-white mt-5 capitalize">{{flashcard?.romanji}}</p>
                <p v-if="show_answer" class="text-gray-500 text-xl text-center mt-2 ">{{flashcard?.answer}}</p>
                <div class="flex justify-between mt-12">
                     <button @click.prevent="show_answer = true" type="button" class="inline-flex items-center  text-white bg-brand hover:bg-brand-strong box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">
